dd84a077272efe6e6be9a51826605dd16a039de5
Emmanuel Saura <emmanuelsaura007@gmail.com>
2022-06-20 13:21:21 +0300

    madlib

---
+++ madlib/madlib.py
@@ -0,0 +1,5 @@
+color = input("enter a color : \n")
+plural_noun = input("enter a plural noun : \n")
+celeb = input("enter a celebrity name : \n")
+print("roses are {}, \n {} are blue, \n i love {}".format(color, plural_noun, celeb))
+
